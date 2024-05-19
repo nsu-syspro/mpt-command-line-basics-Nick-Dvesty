433a661bae22aee4a8c27936fe2a5a9144db97f1
Nick-Dvesty <genmeln05@gmail.com>
2024-05-20 02:35:31 +0700

    fix one

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,9 @@
 temp_foldren=`mktemp -d`
-echo $temp_foldren
+this_foldren=`pwd`
 unzip -d $temp_foldren ../data/archive-part2.zip
 tar -xvf ../data/archive-part1.tar -C $temp_foldren
-tar -czvf ../data/archive-combined.tar.gz $temp_foldren/some
-rm -r  $temp_foldren
+cd $temp_foldren
+tar -czvf ../archive-combined.tar.gz .
+cd $this_foldren
+mv $temp_foldren/../archive-combined.tar.gz ../data/
+rm -r $temp_foldren
